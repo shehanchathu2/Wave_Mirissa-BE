fa3cd019782170cb2c40a992cd8f86ea2e0e6d11
shehan <168344942+shehanchathu2@users.noreply.github.com>
2025-08-21 01:20:57 +0530

    add personalize field

--- wave-Mirissa/src/main/java/com/wave/Mirissa/services/ProductService.java
+++ wave-Mirissa/src/main/java/com/wave/Mirissa/services/ProductService.java
@@ -70,6 +70,11 @@ public class ProductService {
         }else if (products instanceof Anklet) {
             products.setTypeForDb("anklet");
         }
+
+        if (products.getPersonalize() == null || products.getPersonalize().isBlank()) {
+            products.setPersonalize("none"); // default value if nothing sent
+        }
+
         return productRepository.save(products);
     }
 
@@ -99,7 +104,9 @@ public class ProductService {
         existing.setGender(updatedProduct.getGender());
         existing.setCustomization(updatedProduct.getCustomization());
         existing.setAvailable(updatedProduct.isAvailable());
-
+        existing.setSkinToneTags(updatedProduct.getSkinToneTags());
+        existing.setFaceShapeTags(updatedProduct.getFaceShapeTags());
+        existing.setPersonalize(updatedProduct.getPersonalize());
 
 
         existing.setImageUrl1(updatedProduct.getImageUrl1());
@@ -126,6 +133,8 @@ public class ProductService {
             existing.setTypeForDb(null);
         }
 
+        System.out.println("FaceShape: " + updatedProduct.getFaceShapeTags());
+        System.out.println("SkinTone: " + updatedProduct.getSkinToneTags());
 
         return productRepository.save(existing);
     }
